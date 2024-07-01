6e1c010a4d2d07c27101b17fe4ca3abf21a06247
Anish Kumar SS <ssanish686@gmail.com>
2024-07-01 15:08:12 +0530

    Initial Commit

---
+++ AuthApplication/AuthApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthApplicationApp.swift
+//  AuthApplication
+//
+//  Created by Anish Kumar SS on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AuthApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AuthApplication
+//
+//  Created by Anish Kumar SS on 01/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
